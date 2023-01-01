ed8b337561ff0c0eaa3c43a40489291bdb97b394
Jason Russell <grayhatguy@grayhatguy.com>
2023-01-01 17:51:13 -0500

    Create dqd.sh

---
+++ bin/dqd.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo "Enter Your Name"
+read name
+echo "Welcome $name to LinuxHint"
+## dockery - bash scripts for improving docker workflows
+	## dbu.sh 
+	## make backup directory in ~/ for images and routes
+	cd ~/ & mkdir /dockbkup ; cd /dockbkup && mkdir img && cd .. ; mkdir routes 
+	## save images
+	docker save $(docker images -q) -o ~/dockbkup/img/dockerimages`date +%d%b%Y`.tar
+	## save routes
+	iptables-save > savedrules.txt 
+	## save volumes 
+	## TBD - add loop opens volume /dbdata on dbstore for backup on localhost /backup
+	## docker run -v /dbdata --name dbstore ubuntu /bin/bash
+	## docker run --rm --volumes-from dbstore -v $(pwd):/backup ubuntu tar cvf /backup/backup.tar /dbdata
+	## (dnew.sh only)
+	## Add pause and y or n 
+	## dcl.sh
+	## clear docker
+	docker container stop $(docker container ls -aq) && docker container rm -f $(docker container ls -aq) && docker rmi -f $(docker images -aq) && docker volume prune && docker network prune
+	## drm.sh 
+	## Remove everything docker 
+	sudo dpkg -l | grep -i docker ; sudo apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli docker-compose-plugin && sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce docker-compose-plugin sudo rm -rf /var/lib/docker /etc/docker && sudo rm /etc/apparmor.d/docker && sudo groupdel docker && sudo rm -rf /var/run/docker.sock
+	## ddn.sh
+	## quick install see https://getdocker.com for full install
+	curl -fsSL https://get.docker.com -o get-docker.sh
+	## dck,sh
+	## check install versions upgrade
+	docker compose version && docker version
+	sudo apt-get upgrade -y && sudo apt-get update -y
+	## dhi.sh
+	hello world 
