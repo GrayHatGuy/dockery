ba8d23c4f1ad9524981c5c2437be6557caac2b15
Jason Russell <grayhatguy@grayhatguy.com>
2023-01-01 18:54:06 -0500

    Update dqd.sh

--- bin/dqd.sh
+++ bin/dqd.sh
@@ -11,7 +11,12 @@ iptables-save > ~/dockbkup/img/savedrules`date +%d%b%Y`.txt
 echo "Images and Routes are backed up to ~/dockbkup/"
 echo "Continue clearing docker? y/n or <ctrl> + c to quit"
 read answer1
-
+if [[ ( $answer1 == "n" ) ]]; then
+echo "user exit"
+exit
+else
+echo "Clearing docker now..."
+fi
 ## dcl.sh
 ## clear docker
 docker container stop $(docker container ls -aq) && docker container rm -f $(docker container ls -aq) && docker rmi -f $(docker images -aq) && docker volume prune && docker network prune
@@ -19,9 +24,23 @@ echo "docker cleared!"
 docker ps
 echo "Do you want to remove everything docker including install? y/n or <ctrl> + c to quit"
 read answer2
+if [[ ( $answer2 == "n" ) ]]; then
+echo "user exit"
+exit
+else
+echo "Nuking everything docker now..."
+fi
 ## drm.sh 
 ## Remove everything docker 
 sudo dpkg -l | grep -i docker ; sudo apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli docker-compose-plugin && sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce docker-compose-plugin sudo rm -rf /var/lib/docker /etc/docker && sudo rm /etc/apparmor.d/docker && sudo groupdel docker && sudo rm -rf /var/run/docker.sock
+echo "Do you want to re-install docker? y/n or <ctrl> + c to quit"
+read answer3
+if [[ ( $answer3 == "n" ) ]]; then
+echo "user exit"
+exit
+else
+echo "Re-install docker..."
+fi
 ## ddn.sh
 ## quick install see https://getdocker.com for full install
 curl -fsSL https://get.docker.com -o get-docker.sh
