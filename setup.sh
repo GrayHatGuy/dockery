27f5c4aa6b490164f6c34a51b32c16764c86f650
Jason Russell <grayhatguy@grayhatguy.com>
2023-01-01 18:41:04 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 #!/bash/bin
-
+## go $HOME
 cd ~/
-
-Clone the repo.
-
+## clone git
 git clone https://github.com/GrayHatGuy/dockery.git
-
-Add scripts to PATH
-
+echo "cloned dockery"
+## add $PATH
 if [ -d "$HOME/bin" ] ; then
   PATH="$PATH:$HOME/dockery/bin"
 fi
+echo "added to $PATH"
