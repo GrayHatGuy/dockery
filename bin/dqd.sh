81473d1af3083c6bca447bec9cad5e7dad6e2d43
Jason Russell <grayhatguy@grayhatguy.com>
2023-01-01 19:23:12 -0500

    Update dqd.sh

--- bin/dqd.sh
+++ bin/dqd.sh
@@ -1,27 +1,12 @@
 #!/bin/bash
-#!/bin/bash
 
 while getopts 'yn:' OPTION; do
   case "$OPTION" in
     y)
-      echo "y flag detected removing data"
+      echo "y flag detected removing ALL data"
       ;;
     n)
       echo "n flag detected skipping all data removal"
-      ;;
-    #!/bin/bash
-
-while getopts 'lha:' OPTION; do
-  case "$OPTION" in
-    l)
-      echo "linuxconfig"
-      ;;
-    h)
-      echo "you have supplied the -h option"
-      ;;
-    a)
-      avalue="$OPTARG"
-      echo "The value provided is $OPTARG"
       ;;
     ?)
       echo "script flag does not exist see usage: $(basename \$0) [-y] [-n]" >&2
@@ -29,8 +14,6 @@ while getopts 'lha:' OPTION; do
       ;;
   esac
 done
-shift "$(($OPTIND -1))"
-
 
 ## dockery - bash scripts for improving docker workflows
 ## dbu.sh 
@@ -41,7 +24,8 @@ docker save $(docker images -q) -o ~/dockbkup/img/dockerimages`date +%d%b%Y`.tar
 ## save routes
 iptables-save > ~/dockbkup/img/savedrules`date +%d%b%Y`.txt 
 echo "Images and Routes are backed up to ~/dockbkup/"
-echo "Continue clearing docker? y/n or <ctrl> + c to quit"
+
+echo "Continue clearing docker? y/n"
 read answer1
 if [[ ( $answer1 == "n" ) ]]; then
 echo "user exit"
@@ -49,11 +33,13 @@ exit
 else
 echo "Clearing docker now..."
 fi
+
 ## dcl.sh
 ## clear docker
 docker container stop $(docker container ls -aq) && docker container rm -f $(docker container ls -aq) && docker rmi -f $(docker images -aq) && docker volume prune && docker network prune
 echo "docker cleared!"
 docker ps
+
 echo "Do you want to remove everything docker including install? y/n or <ctrl> + c to quit"
 read answer2
 if [[ ( $answer2 == "n" ) ]]; then
@@ -62,9 +48,11 @@ exit
 else
 echo "Nuking everything docker now..."
 fi
+
 ## drm.sh 
 ## Remove everything docker 
 sudo dpkg -l | grep -i docker ; sudo apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli docker-compose-plugin && sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce docker-compose-plugin sudo rm -rf /var/lib/docker /etc/docker && sudo rm /etc/apparmor.d/docker && sudo groupdel docker && sudo rm -rf /var/run/docker.sock
+
 echo "Do you want to re-install docker? y/n or <ctrl> + c to quit"
 read answer3
 if [[ ( $answer3 == "n" ) ]]; then
@@ -73,6 +61,7 @@ exit
 else
 echo "Re-install docker..."
 fi
+
 ## ddn.sh
 ## quick install see https://getdocker.com for full install
 curl -fsSL https://get.docker.com -o get-docker.sh
