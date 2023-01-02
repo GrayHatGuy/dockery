fa589cb1f2943fd7bc3552fd7dbb8dfb67293e82
Jason Russell <grayhatguy@grayhatguy.com>
2023-01-02 03:23:10 -0500

    Create dok.sh

---
+++ bin/dok.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+## ~/dockery/bin/dok.sh
+echo "Starting " $(basename) 
+## dok.sh - docker install verify status
+docker ps && && docker images && docker volume ls && docker networks ls && sudo iptables -L  
+if [ $? -eq 0 ] 
+then 
+  echo "docker is good to go!'" 
+else 
+  echo "docker failed status check error status return for dok.sh script" >&2 
+fi
