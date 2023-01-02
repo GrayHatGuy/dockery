eeb1248bb94ea22b94f4628cad644536e82fb9db
Jason Russell <grayhatguy@grayhatguy.com>
2023-01-02 16:11:14 -0500

    Update dqd.sh

--- bin/dqd.sh
+++ bin/dqd.sh
@@ -78,7 +78,7 @@ done
 echo "Hello-world starting rn..."
 docker run hello-world 
 ## dok.sh - docker install status check
-docker ps && && docker images && docker volume ls && docker networks ls && sudo iptables -L  
+docker ps && docker images && docker volume ls && docker networks ls && sudo iptables -L  
 if [ $? -eq 0 ] 
 then 
   echo "docker is good to go!'" 
