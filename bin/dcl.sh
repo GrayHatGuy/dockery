bf75f3069f6d19cdffb1f340e4c0d5d6dfa54674
Jason Russell <grayhatguy@grayhatguy.com>
2023-01-02 03:03:45 -0500

    Update dcl.sh

--- bin/dcl.sh
+++ bin/dcl.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+## ~/dockery/bin/dqd.sh
+echo "Starting " $(basename) 
+echo "Clearing docker do you wish to continue? y/n" && read abort1
+if [[ ( $abort1 == "n" ) ]]; then
+echo "*user abort*"
+exit 1
+else
+echo "*User confirmed data removal.*"
+## dcl.sh clear docker stop containers remove images prune network and volumes
+echo "*-y flag clearing docker...*" ; docker container stop $(docker container ls -aq) && docker container rm -f $(docker container ls -aq) && docker rmi -f $(docker images -aq) && docker volume prune && docker network prune && echo "...cleared!" ; docker ps
+fi
 
