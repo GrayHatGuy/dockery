3b1d2fb1a01e2dc48575e49389452f2c5bb46ebd
Jason Russell <grayhatguy@grayhatguy.com>
2023-01-02 03:17:59 -0500

    Update drm.sh

--- bin/drm.sh
+++ bin/drm.sh
@@ -3,7 +3,7 @@
 echo "Starting " $(basename) 
 echo "Continue docker nuke and reinstall? y/n" && read answer2
 if [[ ( $answer2 == "n" ) ]]; then
-echo "*user select skipping docker nuke*"
+echo "*no docker nuke for you!*"
 else
 ## ddn.sh using quick install see https://getdocker.com for full install apt refresh reinstall docker add $USER to group docker enable systemctl
 echo "*user select docker nuke* Now nuking docker..." ; sudo dpkg -l | grep -i docker ; sudo apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli docker-compose-plugin && sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce docker-compose-plugin sudo rm -rf /var/lib/docker /etc/docker && sudo rm /etc/apparmor.d/docker && sudo groupdel docker && sudo rm -rf /var/run/docker.sock && echo "...nuked!!!" 
