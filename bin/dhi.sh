1b0f84861b1a7ea1d1dad5eb9cc5f3b8bc595cee
Jason Russell <grayhatguy@grayhatguy.com>
2023-01-02 04:08:00 -0500

    Update dhi.sh

--- bin/dhi.sh
+++ bin/dhi.sh
@@ -1,1 +1,13 @@
 
+
+## dhi.sh - docker hello world
+echo "Hello-world starting rn..."
+docker run hello-world 
+## dok.sh - docker install status check
+docker ps && && docker images && docker volume ls && docker networks ls && sudo iptables -L  
+if [ $? -eq 0 ] 
+then 
+  echo "docker is good to go!'" 
+else 
+  echo "docker failed status check error status return for dhi.sh script" >&2 
+fi
