da57d5496637ff0cf8f95285fd38a80985ce30d6
Jason Russell <grayhatguy@grayhatguy.com>
2023-01-03 01:11:51 -0500

    Update drm.sh

--- bin/drm.sh
+++ bin/drm.sh
@@ -6,7 +6,7 @@ if [[ ( $answer2 == "n" ) ]]; then
 echo "*no nuke for you!*"
 else
 ## ddn.sh using quick install see https://getdocker.com for full install apt refresh reinstall docker add $USER to group docker enable systemctl
-echo "*user selected docker nuke* - nuking docker..." ; sudo dpkg -l | grep -i docker ; sudo apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli docker-compose-plugin && sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce docker-compose-plugin && sudo rm -rf /var/lib/docker /etc/docker && sudo rm /etc/apparmor.d/docker && sudo groupdel docker && sudo rm -rf /var/run/docker.sock && echo "...nuked!!!" 
+echo "*user selected docker nuke* - nuking docker..." ; sudo dpkg -l | grep -i docker ; sudo apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli docker-compose-plugin && sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce docker-compose-plugin && sudo rm -rf /var/lib/docker /etc/docker && sudo rm -rf /etc/apparmor.d/docker && sudo groupdel docker && sudo rm -rf /var/run/docker.sock && echo "...nuked!!!" 
 retVal=$?
 if [ $retVal -ne 0 ]; then
 echo "Error"
