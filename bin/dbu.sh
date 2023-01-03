205ce3e2ed0955f48377658677491c70df08d950
Jason Russell <grayhatguy@grayhatguy.com>
2023-01-03 01:06:21 -0500

    Update dbu.sh

--- bin/dbu.sh
+++ bin/dbu.sh
@@ -5,7 +5,7 @@ echo "Starting "
 echo "create directories ~/dockbkup for images and routes" 
 cd $HOME && sudo mkdir -m777 ~/dockbkup ; cd ~/dockbkup && sudo mkdir -m777 img && sudo mkdir -m777 routes  
 echo "Backing up..."
-docker save $(docker images -q) -o ~/dockbkup/img/dockerimages`date +%d%b%Y`.tar ; iptables-save > ~/dockbkup/img/savedrules`date +%d%b%Y`.txt && echo "Images and routes backed up to ~/dockbkup/"
+docker save $(docker images -q) -o ~/dockbkup/img/dockerimages`date +%d%b%Y`.tar ; sudo iptables-save > ~/dockbkup/img/savedrules`date +%d%b%Y`.txt && echo "Images and routes backed up to ~/dockbkup/"
 retVal=$?
 if [ $retVal -ne 0 ]; then
 echo "Error"
