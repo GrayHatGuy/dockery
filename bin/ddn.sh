7fd5059bb2a99c8175e2a98aab2555972cc6ea5e
Jason Russell <grayhatguy@grayhatguy.com>
2023-01-03 10:08:32 -0500

    Update ddn.sh
    remove systemctl restart docker from ddn.sh

--- bin/ddn.sh
+++ bin/ddn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## ~/dockery/bin/ddn.sh
 echo "Starting "   
-echo "re-installing..." ; sudo apt-get update -y && sudo apt-get upgrade -y ; sudo curl -fsSL https://get.docker.com -o get-docker.sh && sudo bash get-docker.sh && sudo apt-get upgrade -y && sudo apt-get update -y ; sudo usermod -aG docker $USER ; sudo systemctl enable docker.service && sudo systemctl enable containerd.service && sudo systemctl restart docker && echo "docker installed!"
+echo "re-installing..." ; sudo apt-get update -y && sudo apt-get upgrade -y ; sudo curl -fsSL https://get.docker.com -o get-docker.sh && sudo bash get-docker.sh && sudo apt-get upgrade -y && sudo apt-get update -y ; sudo usermod -aG docker $USER ; sudo systemctl enable docker.service && sudo systemctl enable containerd.service && echo "docker installed!"
 retVal=$?
 if [ $retVal -ne 0 ]; then
 echo "Error"
